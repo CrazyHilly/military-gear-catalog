bb49e2685729a9afc32e5bf046ef5a4dab6019c5
CrazyHilly <juliadudych@gmail.com>
2025-04-09 12:39:49 +0300

    Add view to display a list of products

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,35 @@
+from django.db.models import Q
 from django.views import generic
 
+from catalog.forms import ProductSearchForm
 from catalog.models import Product
 
 
 class ProductDetailView(generic.DetailView):
     model = Product
+
+
+class ProductListView(generic.ListView):
+    model = Product
+    paginate_by = 20
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_input = self.request.GET.get("search_input")
+        if search_input:
+            queryset = queryset.filter(
+                Q(name__icontains=search_input) |
+                Q(country__ua_name__icontains=search_input) |
+                Q(country__en_name__icontains=search_input) |
+                Q(description__icontains=search_input) |
+                Q(product_number__icontains=search_input)
+            )
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        search_input = self.request.GET.get("search_input", "")
+        context["search_form"] = ProductSearchForm(
+            initial={"search_input": search_input}
+        )
+        return context
