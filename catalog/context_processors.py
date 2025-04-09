e1fa4dee79de7c92160cd73cf619950b01c52d4f
CrazyHilly <juliadudych@gmail.com>
2025-04-09 19:50:08 +0300

    Add context processor for customer wishlist

--- catalog/context_processors.py
+++ catalog/context_processors.py
@@ -13,3 +13,11 @@ def countries_context(request):
         ).filter(product_count__gt=0).values(*fields_to_display))
         cache.set("countries_with_products", cached_data, 600)
     return {"countries_with_products": cached_data}
+
+
+def customer_wishlist_context(request):
+    user = request.user
+    product_nums = []
+    if user.is_authenticated:
+        product_nums = user.wishlist.values_list("product_number", flat=True)
+    return {"customer_wishlist_product_numbers": product_nums}
