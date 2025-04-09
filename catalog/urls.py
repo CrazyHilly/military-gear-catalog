ca5486626998e9c3037d55772fed9df1fdbb52b1
CrazyHilly <juliadudych@gmail.com>
2025-04-09 14:51:28 +0300

    Add URLs for catalog views

---
+++ catalog/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from catalog.views import (
+    ProductListView, ClothingListView, FootwearListView, AccessoryListView,
+    ProductDetailView, CountryListView, CountryProductsListView,
+    CustomerDetailView, CustomerWishlistView, update_wishlist
+)
+
+urlpatterns = [
+    path("", ProductListView.as_view(), name="product-list"),
+    path("product/<slug:slug>/", ProductDetailView.as_view(), name="product-detail"),
+    path("product/<int:product_number>/update-wishlist", update_wishlist, name="update-wishlist"),
+    path("clothing/", ClothingListView.as_view(), name="clothing-list"),
+    path("footwear/", FootwearListView.as_view(), name="footwear-list"),
+    path("accessories/", AccessoryListView.as_view(), name="accessory-list"),
+    path("countries/", CountryListView.as_view(), name="country-list"),
+    path("countries/<str:name>/", CountryProductsListView.as_view(), name="country-products-list"),
+    path("customer/", CustomerDetailView.as_view(), name="customer-detail"),
+    path("customer/wishlist/", CustomerWishlistView.as_view(), name="customer-wish-list"),
+]
+
+app_name = "catalog"
