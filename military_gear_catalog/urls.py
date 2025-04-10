020ab53208056a4b2fa432c9d5fc5352e4bd964e
CrazyHilly <juliadudych@gmail.com>
2025-04-10 13:54:47 +0300

    Add project URLs for admin, registration, app, debug, views

---
+++ military_gear_catalog/urls.py
@@ -0,0 +1,28 @@
+import debug_toolbar
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf import settings
+
+from catalog.views import ProductListView
+from military_gear_catalog.views import (
+    contacts_view, how_to_order_view, about_us_view
+)
+
+urlpatterns = [
+    path("", ProductListView.as_view(), name="product-list"),
+    path("admin/", admin.site.urls),
+    path("catalog/", include("catalog.urls", namespace="catalog")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("__debug__/", include(debug_toolbar.urls)),
+    path("contacts/", contacts_view, name="contacts"),
+    path("how-to-order/", how_to_order_view, name="how-to-order"),
+    path("about-us/", about_us_view, name="about-us"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = ([path("__debug__/", include(debug_toolbar.urls)),] +
+                   urlpatterns)
