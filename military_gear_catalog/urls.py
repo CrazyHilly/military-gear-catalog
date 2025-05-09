bc4c9cf12639cc78744e48e56e664586bb2c613b
Yuliia Dudych <juliadudych@gmail.com>
2025-05-09 20:59:06 +0300

    Add location of media files to the urls

--- military_gear_catalog/urls.py
+++ military_gear_catalog/urls.py
@@ -10,16 +10,18 @@ from military_gear_catalog.views import (
     contacts_view, how_to_order_view, about_us_view
 )
 
-urlpatterns = [
-    path("", ProductListView.as_view(), name="product-list"),
-    path("admin/", admin.site.urls),
-    path("catalog/", include("catalog.urls", namespace="catalog")),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("__debug__/", include(debug_toolbar.urls)),
-    path("contacts/", contacts_view, name="contacts"),
-    path("how-to-order/", how_to_order_view, name="how-to-order"),
-    path("about-us/", about_us_view, name="about-us"),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = ([
+        path("", ProductListView.as_view(), name="product-list"),
+        path("admin/", admin.site.urls),
+        path("catalog/", include("catalog.urls", namespace="catalog")),
+        path("accounts/", include("django.contrib.auth.urls")),
+        path("__debug__/", include(debug_toolbar.urls)),
+        path("contacts/", contacts_view, name="contacts"),
+        path("how-to-order/", how_to_order_view, name="how-to-order"),
+        path("about-us/", about_us_view, name="about-us"),
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
 
 
 if settings.DEBUG:
