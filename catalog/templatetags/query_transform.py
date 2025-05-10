0512e615e3708e1a2d050dc3cc7fc34d9eacafa1
Yuliia Dudych <juliadudych@gmail.com>
2025-05-10 18:47:03 +0300

    Add templatetag to shorten the urls

--- catalog/templatetags/query_transform.py
+++ catalog/templatetags/query_transform.py
@@ -1,4 +1,6 @@
 from django import template
+from django.urls import reverse
+from urllib.parse import urlencode
 
 register = template.Library()
 
@@ -13,3 +15,19 @@ def query_transform(request, **kwargs):
             updated.pop(key, 0)
 
     return updated.urlencode()
+
+
+@register.simple_tag(takes_context=True)
+def update_wishlist_url(context, product, action=None):
+    product_number = product.product_number
+    base_url = reverse(
+        "catalog:update-wishlist", 
+        kwargs={"product_number": product_number}
+        )
+    next_path = context["request"].path
+
+    query_params = {"next": next_path}
+    if action:
+        query_params["action"] = action
+
+    return f"{base_url}?{urlencode(query_params)}"
