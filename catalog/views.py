e9d3c4762c3fa82fac2edfd101b649b64c327301
Julia Dudych <juliadudych@gmail.com>
2025-05-14 18:02:39 +0300

    Modify product list page layout

--- catalog/views.py
+++ catalog/views.py
@@ -16,7 +16,7 @@ class ProductDetailView(generic.DetailView):
 
 class ProductListView(generic.ListView):
     model = Product
-    paginate_by = 6
+    paginate_by = 8
 
     def get_queryset(self):
         queryset = super().get_queryset()
