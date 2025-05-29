89c323210f6f67ea533f0c18b22c021a9010f0b4
Julia Dudych <juliadudych@gmail.com>
2025-05-29 19:01:26 +0300

    Create Admin page; add Product model

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from catalog.models import Product, ProductImage
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "product_number", 
+        "name", 
+        "display_category", 
+        "country", 
+        "price_low", 
+        "price_high", 
+        "description", 
+        "available"
+        ]
+    search_fields = [
+        "name", 
+        "country__ua_name", 
+        "country__en_name", 
+        "description", 
+        "product_number", 
+        "category"
+        ]
+    list_filter = ["available", "country__ua_name", "category"]
+    inlines = [ProductImageInline]
+
+    def display_category(self, obj):
+        return f"{obj.category} - {obj.get_category_display()}"
+    
+    display_category.short_description = "Категорія"
+
+    exclude = ["slug"]
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ["product_number"]
+        return []
+
+    def get_fields(self, request, obj=None):
+        fields = super().get_fields(request, obj)
+        if not obj:
+            fields = [f for f in fields if f != "product_number"]
+        return fields
+
+    actions = ["change_availability"]
+
+    @admin.action(description=_("Змінити наявність"))
+    def change_availability(self, request, queryset):
+        for item in queryset:
+            item.available=not item.available
+            item.save()
+
+
+admin.site.unregister(Group)
