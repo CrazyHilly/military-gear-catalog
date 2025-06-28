cd2e1001a0561eea36aa13dcd27285f99aa375b2
Julia Dudych <juliadudych@gmail.com>
2025-06-28 21:11:52 +0300

    Update country list to be sorted by a Ukrainian alphabet

--- catalog/context_processors.py
+++ catalog/context_processors.py
@@ -1,16 +1,27 @@
+import locale
+
 from django.core.cache import cache
 from django.db.models import Count
 
 from .models import Country
 
+locale.setlocale(locale.LC_COLLATE, 'uk_UA.UTF-8')
+
 
 def countries_context(request):
     fields_to_display = ("id", "ua_name", "en_name", "product_count")
+
     cached_data = cache.get("countries_with_products")
     if cached_data is None:
-        cached_data = list(Country.objects.annotate(
+        countries_with_products = Country.objects.annotate(
             product_count=Count("products")
-        ).filter(product_count__gt=0).values(*fields_to_display))
+            ).filter(product_count__gt=0).values(*fields_to_display)
+
+        cached_data = sorted(
+            countries_with_products, 
+            key=lambda c: locale.strxfrm(c["ua_name"])
+            )
+        
         cache.set("countries_with_products", cached_data, 600)
     return {"countries_with_products": cached_data}
 
