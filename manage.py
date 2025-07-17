99dad32283091cd26c51e5db504ce5a81750456e
Julia Dudych <juliadudych@gmail.com>
2025-07-17 21:52:56 +0300

    Prepare project files for production

--- manage.py
+++ manage.py
@@ -11,7 +11,7 @@ load_dotenv()
 def main():
     """Run administrative tasks."""
     os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "military_gear_catalog.settings.dev"
+        "DJANGO_SETTINGS_MODULE", "military_gear_catalog.settings.prod"
     )
     try:
         from django.core.management import execute_from_command_line
