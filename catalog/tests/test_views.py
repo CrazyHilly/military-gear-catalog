d4adb85919e91f5cd6499fe14276a870815fe8a8
Julia Dudych <juliadudych@gmail.com>
2025-08-04 22:14:28 +0300

    Add tests for country products list view

--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -179,3 +179,74 @@ class CountryListViewTest(TestCase):
             len(response.context[self.view]),
             Country.objects.count() - self.num_per_page
         )
+
+
+class CountryProductsListViewTest(TestCase):
+    def setUp(self):
+        self.num_per_page = ProductListView.paginate_by
+        self.view = "product_list"
+
+        self.country = Country.objects.create(en_name="country", ua_name="країна")
+        self.product = Clothing.objects.create(
+            name="одяг",
+            country=self.country,
+            price_low=100,
+            price_high=200,
+        )
+        country = Country.objects.create(en_name="test", ua_name="тест")
+        for _ in range(self.num_per_page + 1):
+            Clothing.objects.create(
+            name="одяг",
+            country=country,
+            price_low=1,
+            price_high=2,
+        )
+            
+        self.url = reverse(
+            f"catalog:country-products-list", 
+            kwargs={"name": self.country.en_name}
+            )
+        self.response = self.client.get(self.url)
+        self.template = f"catalog/{self.view}.html"
+
+    def test_country_product_list_view_is_accessible(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_country_product_list_view_uses_correct_template(self):
+        self.assertTemplateUsed(self.response, self.template)
+
+    def test_country_product_list_pagination_is_correct(self):
+        self.assertFalse(self.response.context["is_paginated"])
+        self.assertEqual(len(self.response.context[self.view]), 1)
+
+        url = reverse(f"catalog:country-products-list", kwargs={"name": "Test"})
+        response = self.client.get(url)
+        self.assertTrue(response.context["is_paginated"])
+        self.assertEqual(len(response.context[self.view]), self.num_per_page)
+
+    def test_country_product_list_view_displays_all_items(self):
+        url = reverse(f"catalog:country-products-list", kwargs={"name": "Test"})
+        response = self.client.get(url)
+        num_pages = response.context["paginator"].num_pages
+        response = self.client.get(url + f"?page={num_pages}")
+        self.assertEqual(len(response.context[self.view]), 1)
+
+    def test_country_product_list_view_search_displays_correct_results(self):
+        response = self.client.get(self.url + f"?search_input=одяг")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context[self.view]), 1)
+
+        url = reverse(f"catalog:country-products-list", kwargs={"name": "Test"})
+        response = self.client.get(url + f"?search_input=одяг")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context[self.view]), self.num_per_page)
+
+    def test_country_product_list_view_context_is_correct(self):
+        self.assertIn("search_scope", self.response.context)
+        form = self.response.context["search_form"]
+        self.assertEqual(form.initial.get("search_scope"), "category")
+
+        response = self.client.get(self.url + f"?search_input=одяг")
+        self.assertIn("search_form", response.context)
+        form = response.context["search_form"]
+        self.assertEqual(form.initial.get("search_input"), "одяг")
